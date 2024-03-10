7536976e0e4a971a68a5f140664f9da4ddcd2654
Peter Stuifzand <peter@madebymouses.com>
2024-03-10 16:20:01 +0100

    Changes

--- init.lua
+++ init.lua
@@ -751,6 +751,33 @@ require('lazy').setup {
           { name = 'path' },
         },
       }
+
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'git' }
+        }, {
+            { name = 'buffer' }
+          })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+          })
+      })
+
     end,
   },
 
